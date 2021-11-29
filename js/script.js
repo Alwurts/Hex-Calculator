7da0be92b6640b0461b4e2f38a28ab03fa0c6e40
Alwurts <32560488+Alwurts@users.noreply.github.com>
2021-11-29 13:07:23 -0600

    Service worker start from scratch

--- js/script.js
+++ js/script.js
@@ -1,22 +1,4 @@
 
-// Event listener to load the serviceworker
-window.addEventListener('load', () => {
-  registerSW();
-});
-
-// Service worker for pwa functionality
-async function registerSW() {
-  if ('serviceWorker' in navigator) {
-    try {
-      await navigator
-            .serviceWorker
-            .register('serviceworker.js');
-    }
-    catch (e) {
-      console.log('SW registration failed');
-    }
-  }
-}
 
 
 
